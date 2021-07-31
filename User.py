32952d0ffa09656c58f84073476ff492bcff8b12
samwel-chege <samwelchegeh09@gmail.com>
2021-07-31 11:12:59 +0300

    add pycache file

--- User.py
+++ User.py
@@ -1,4 +1,16 @@
 class User:
     '''
-    Class that generates new instances of contacts 
+    Class that generates new instances of users 
     '''
+    user_list = []
+    def __init__(self,first_name,last_name,passcode):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.passcode = passcode
+
+    def save_account(self):
+        '''
+        save_account method saves objects into user_list
+        ''' 
+        User.user_list.append(self)   
--- User_test.py
+++ User_test.py
@@ -10,8 +10,28 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test case 
     '''
 
-def setUp(self):
-    '''
-    Set up method to run before each test cases 
-    '''
-    self.new_account = User("Sammie", "Sam", "3421")
+    def setUp(self):
+        '''
+        Set up method to run before each test cases 
+        '''
+        self.new_account = User("Sammie", "Sam", "3421")
+
+    def test_init(self):
+        '''
+        test_init case to test if the object id initialized properly 
+        '''
+
+        self.assertEqual(self.new_account.first_name, "Sammie")
+        self.assertEqual(self.new_account.last_name, "Sam")
+        self.assertEqual(self.new_account.passcode, "3421")
+
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the user object is saved into the user list
+        ''' 
+        self.new_account.save_account()
+        self.assertEqual(len(User.user_list),1)   
+
+
+if __name__ == '__main__':
+    unittest.main()        
