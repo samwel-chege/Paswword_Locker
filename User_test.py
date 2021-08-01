95eba04935a55f4bfb173b4dac2c22e890c8796c
samwel-chege <samwelchegeh09@gmail.com>
2021-08-01 17:40:00 +0300

    create a save account test

--- User_test.py
+++ User_test.py
@@ -1,6 +1,7 @@
 import unittest
 from User import User
 
+
 class TestUser(unittest.TestCase):
 
     '''
@@ -14,7 +15,7 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases 
         '''
-        self.new_account = User("Sammie", "Sam", "Twitter", "3421")
+        self.new_account = User("Sammie", "Sam","3421")
 
     def tearDown(self):
         '''
@@ -30,7 +31,6 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(self.new_account.first_name, "Sammie")
         self.assertEqual(self.new_account.last_name, "Sam")
-        self.assertEqual(self.new_account.sitename,"Twitter")
         self.assertEqual(self.new_account.passcode, "3421")
 
     def test_save_account(self):
@@ -40,57 +40,6 @@ class TestUser(unittest.TestCase):
         self.new_account.save_account()
         self.assertEqual(len(User.user_list),1)  
 
-    def test_save_multiple_account(self):
-        '''
-        test save_multiple_accounts to check if we can save multiple account objects to our user_list
-        '''
-        self.new_account.save_account()
-        test_account = User("Test","user", "Twitter", "3421")
-        test_account.save_account()
-        self.assertEqual(len(User.user_list),2)
-
-    def test_delete_account(self):
-        '''
-        test delete_account to test if we can remove an account from our user list 
-        '''  
-        self.new_account.save_account()
-        test_account = User("Test", "user", "Twitter", "3421")
-        test_account.save_account()
-
-        self.new_account.delete_account()
-        self.assertEqual(len(User.user_list),1)
-
-    def test_account_exists(self):
-        '''
-        test to check if we can return a Boolean if we cannot find the account
-        ''' 
-        self.new_account.save_account()
-        test_account = User("Test", "user", "Twitter", "3421")
-        test_account.save_account()
-
-        account_exists = User.account_exist("Twitter")  
-
-        self.assertTrue(account_exists)  
-
-    def test_find_account_by_sitename(self):
-        '''
-        test to check if we can find an account and display information using that sitename
-        '''  
-        self.new_account.save_account()
-        test_account = User("Test", "user", "Twitter","3421")
-        test_account.save_account()
-
-        found_account = User.find_by_sitename("Twitter")  
-
-        self.assertEqual(found_account.passcode,test_account.passcode)
-
-    def test_display_all_accounts(self):
-        '''
-        method that returns a list of all conyacts saved
-        '''  
-
-        self.assertEqual(User.display_account(),User.user_list)  
-
-
+   
 if __name__ == '__main__':
     unittest.main()        
